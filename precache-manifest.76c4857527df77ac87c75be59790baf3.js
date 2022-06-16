98cdfa98d381829fc389ece5b1e5f80f393e500a
samueljanderson756 <68087208+samueljanderson756@users.noreply.github.com>
2022-06-15 21:48:20 -0500

    Updates

---
+++ precache-manifest.76c4857527df77ac87c75be59790baf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e33f0d187f3d9e3810de",
+    "url": "/static/css/main.e52de5d5.chunk.css"
+  },
+  {
+    "revision": "e33f0d187f3d9e3810de",
+    "url": "/static/js/main.da40ed9c.chunk.js"
+  },
+  {
+    "revision": "8d1b24a60328ef7e83b3",
+    "url": "/static/js/runtime~main.fbfcb512.js"
+  },
+  {
+    "revision": "e2e2b4182361a05938e7",
+    "url": "/static/js/2.39ff7a4a.chunk.js"
+  },
+  {
+    "revision": "d0c7ccbb85f77c99a826",
+    "url": "/static/js/3.91095169.chunk.js"
+  },
+  {
+    "revision": "69b015e3e5a35d497ac1993b949ff1e5",
+    "url": "/index.html"
+  }
+];
